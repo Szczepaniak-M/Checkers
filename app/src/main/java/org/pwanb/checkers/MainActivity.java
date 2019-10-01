48cfb79770438785940bd1f1dce3347e7dd3b53f
MichalSz99 <m.szczepaniak99@gmail.com>
2019-10-01 13:02:14 +0200

    Full screen mode action

--- app/src/main/java/org/pwanb/checkers/MainActivity.java
+++ app/src/main/java/org/pwanb/checkers/MainActivity.java
@@ -1,13 +1,11 @@
 package org.pwanb.checkers;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.GestureDetector;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -47,9 +45,14 @@ public class MainActivity extends AppCompatActivity implements NavController.OnD
         NavController navController = Navigation.findNavController(this, R.id.fragment_container);
         navController.addOnDestinationChangedListener(this);
         NavigationUI.setupWithNavController(navigationView, navController);
-        handler.post(new Hide());
         setNavigationViewAction();
         setGestureDetector();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.post(new Hide());
     }
 
     @Override
@@ -60,6 +63,14 @@ public class MainActivity extends AppCompatActivity implements NavController.OnD
 
         } else {
             navigationView.getMenu().findItem(R.id.nav_game_option).setVisible(false);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            handler.post(new Hide());
         }
     }
 
@@ -112,14 +123,22 @@ public class MainActivity extends AppCompatActivity implements NavController.OnD
 
                 return true;
             }
+
             @Override
             public boolean onScroll(MotionEvent e1, MotionEvent e2,
                                     float distanceX, float distanceY)  {
-                Log.i("TAG", "onDoubleTap: ");
-                Log.d("TAG", "onScroll: ");
-                handler.post(new Show());
-                handler.postDelayed(new Hide(),2000);
-                return true;}
+                if(e1.getY() - e2.getY() > 100)  {
+                    handler.removeCallbacksAndMessages(null);
+                    handler.post(new Show());
+                    handler.postDelayed(new Hide(),2500);
+                    return true;
+                } else if (e1.getY() - e2.getY() < -100){
+                    handler.removeCallbacksAndMessages(null);
+                    handler.post(new Hide());
+                    return true;
+                }
+                return false;
+            }
 
         });
 
@@ -137,7 +156,7 @@ public class MainActivity extends AppCompatActivity implements NavController.OnD
         public void run() {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
             getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                             | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
